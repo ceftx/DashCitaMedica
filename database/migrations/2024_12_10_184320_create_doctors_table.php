cb72dcfc82c49826295f71df44e2c8a96d41e65a
ceftx <claudiocmorab@gmail.com>
2024-12-10 15:11:55 -0400

    start

---
+++ database/migrations/2024_12_10_184320_create_doctors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\location;
+use App\Models\specialty;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(location::class);
+            $table->foreignIdFor(specialty::class);
+            $table->string('name');
+            $table->string('phone');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('doctors');
+    }
+};
